43636a27bc9bf5bb8612103c01620b57526eedfc
Romanenko Vladimir <bonyshd@gmail.com>
2026-06-15 13:56:51 +0300

    Сделал домашку

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 import feature
+import feature3 
 print("Hello World")
 #это комментарий
 print(feature.add(1, 2))
 print(feature.subtract(1, 2))
 print(feature.multiply(1, 2))
 print(feature.divide(1, 2))
+
+print(feature3.poo())
