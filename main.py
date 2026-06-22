94b71e0393dfdddb745a7eb111c3ebbafa79d9f8
koreyka-hub <Pobbyazok@gmail.com>
2026-06-22 12:29:26 +0300

    dz egor1

--- main.py
+++ main.py
@@ -3,14 +3,15 @@ from telegram.ext import (
     CommandHandler,
     ConversationHandler,
     CallbackQueryHandler,
+    MessageHandler,
+    filters
 )
 
-
 from config.config import TOKEN
-from config.states import MAINMENU, HELP
+from config.states import MAINMENU, HELP, CREATECHAR, CREATENAME, CREATECLASS, CREATEDESC
 from handlers.start_handler import start_handler
 from handlers.help_handler import help_handler
-    
+from handlers.create_char_handler import create_char_handler_start, create_name, create_class, create_description
 def main():
     import logging
 
@@ -29,14 +30,28 @@ def main():
         states={
             MAINMENU: [
                 CallbackQueryHandler(pattern="help", callback=help_handler),
+                CallbackQueryHandler(pattern="create_character", callback=create_char_handler_start),
             ],
             HELP: [
                 CallbackQueryHandler(pattern="back_to_mainmenu", callback=start_handler),
+            ],
+            CREATECHAR: [
+                CallbackQueryHandler(pattern="back_to_mainmenu", callback=start_handler),
+                CallbackQueryHandler(pattern="create_character_fr", callback=create_name),               
+            ],
+            CREATENAME: [
+                MessageHandler(filters.TEXT & ~filters.COMMAND, callback=create_name),
+            ],
+            CREATECLASS: [
+                MessageHandler(filters.TEXT & ~filters.COMMAND, callback=create_class),
+            ],
+            CREATEDESC: [
+                MessageHandler(filters.TEXT & ~filters.COMMAND, callback=create_description),
             ]
         },
         fallbacks=[CommandHandler("start", start_handler)],
     )
-    #кикишки
+    #кикишки кикиишки
     application.add_handler(conv_handler)
     application.run_polling()
 
